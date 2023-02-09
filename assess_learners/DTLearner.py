31f03689b254f8847e7b7a972eb0556f4f9db08c
MChiang7 <mtchiang117@gmail.com>
2023-02-08 21:47:33 -0500

    init dtlearner

---
+++ assess_learners/DTLearner.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+class DTLearner(object):
+    def __init__(self, leaf_size=1, verbose=False):
+        self.leaf_size = leaf_size
+        self.verbose = verbose
+
+    def author(self):
+        return 'mchiang30'
+
+    def addEvidence(self, data_x, data_y):
+
+        pass
+
+    def query(self, points):
+        pass

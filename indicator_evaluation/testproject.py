887335776250ab89dbdd3463f696410bde72d7d1
MChiang7 <mtchiang117@gmail.com>
2023-03-12 15:31:29 -0400

    test file

---
+++ indicator_evaluation/testproject.py
@@ -0,0 +1,13 @@
+import TheoreticallyOptimalStrategy
+import indicators
+import datetime as dt
+
+def author():
+    return 'mchiang30'
+
+def test_code():
+    tos = TheoreticallyOptimalStrategy.output(100000, dt.datetime(2008, 1, 1), dt.datetime(2009, 12, 31), 'JPM', True, True)
+    #ind = indicators.report()
+
+if __name__ == '__main__':
+    test_code()

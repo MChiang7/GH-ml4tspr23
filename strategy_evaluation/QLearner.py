7e7c7f8feb30aaae21fe86d2efc1e8648a4c2597
MChiang7 <mtchiang117@gmail.com>
2023-04-15 15:46:22 -0400

    updated qlearner

--- strategy_evaluation/QLearner.py
+++ strategy_evaluation/QLearner.py
@@ -1,41 +1,41 @@
-""""""  		  	   		  		 			  		 			     			  	 
+""""""
 """  		  	   		  		 			  		 			     			  	 
 Template for implementing QLearner  (c) 2015 Tucker Balch  		  	   		  		 			  		 			     			  	 
-  		  	   		  		 			  		 			     			  	 
+
 Copyright 2018, Georgia Institute of Technology (Georgia Tech)  		  	   		  		 			  		 			     			  	 
 Atlanta, Georgia 30332  		  	   		  		 			  		 			     			  	 
 All Rights Reserved  		  	   		  		 			  		 			     			  	 
-  		  	   		  		 			  		 			     			  	 
+
 Template code for CS 4646/7646  		  	   		  		 			  		 			     			  	 
-  		  	   		  		 			  		 			     			  	 
+
 Georgia Tech asserts copyright ownership of this template and all derivative  		  	   		  		 			  		 			     			  	 
 works, including solutions to the projects assigned in this course. Students  		  	   		  		 			  		 			     			  	 
 and other users of this template code are advised not to share it with others  		  	   		  		 			  		 			     			  	 
 or to make it available on publicly viewable websites including repositories  		  	   		  		 			  		 			     			  	 
 such as github and gitlab.  This copyright statement should not be removed  		  	   		  		 			  		 			     			  	 
 or edited.  		  	   		  		 			  		 			     			  	 
-  		  	   		  		 			  		 			     			  	 
+
 We do grant permission to share solutions privately with non-students such  		  	   		  		 			  		 			     			  	 
 as potential employers. However, sharing with other current or future  		  	   		  		 			  		 			     			  	 
 students of CS 7646 is prohibited and subject to being investigated as a  		  	   		  		 			  		 			     			  	 
 GT honor code violation.  		  	   		  		 			  		 			     			  	 
-  		  	   		  		 			  		 			     			  	 
+
 -----do not edit anything above this line---  		  	   		  		 			  		 			     			  	 
-  		  	   		  		 			  		 			     			  	 
+
 Student Name: Michael Chiang (replace with your name)  		  	   		  		 			  		 			     			  	 
 GT User ID: mchiang30 (replace with your User ID)  		  	   		  		 			  		 			     			  	 
 GT ID: 903216278 (replace with your GT ID)  		  	   		  		 			  		 			     			  	 
-"""  		  	   		  		 			  		 			     			  	 
-  		  	   		  		 			  		 			     			  	 
-import random as rand  		  	   		  		 			  		 			     			  	 
-  		  	   		  		 			  		 			     			  	 
-import numpy as np  		  	   		  		 			  		 			     			  	 
-  		  	   		  		 			  		 			     			  	 
-  		  	   		  		 			  		 			     			  	 
-class QLearner(object):  		  	   		  		 			  		 			     			  	 
+"""
+
+import random as rand
+
+import numpy as np
+
+
+class QLearner(object):
     """  		  	   		  		 			  		 			     			  	 
     This is a Q learner object.  		  	   		  		 			  		 			     			  	 
-  		  	   		  		 			  		 			     			  	 
+
     :param num_states: The number of states to consider.  		  	   		  		 			  		 			     			  	 
     :type num_states: int  		  	   		  		 			  		 			     			  	 
     :param num_actions: The number of actions available..  		  	   		  		 			  		 			     			  	 
@@ -52,24 +52,25 @@ class QLearner(object):
     :type dyna: int  		  	   		  		 			  		 			     			  	 
     :param verbose: If “verbose” is True, your code can print out information for debugging.  		  	   		  		 			  		 			     			  	 
     :type verbose: bool  		  	   		  		 			  		 			     			  	 
-    """  		  	   		  		 			  		 			     			  	 
-    def __init__(  		  	   		  		 			  		 			     			  	 
-        self,  		  	   		  		 			  		 			     			  	 
-        num_states=100,  		  	   		  		 			  		 			     			  	 
-        num_actions=4,  		  	   		  		 			  		 			     			  	 
-        alpha=0.2,  		  	   		  		 			  		 			     			  	 
-        gamma=0.9,  		  	   		  		 			  		 			     			  	 
-        rar=0.5,  		  	   		  		 			  		 			     			  	 
-        radr=0.99,  		  	   		  		 			  		 			     			  	 
-        dyna=0,  		  	   		  		 			  		 			     			  	 
-        verbose=False,  		  	   		  		 			  		 			     			  	 
-    ):  		  	   		  		 			  		 			     			  	 
+    """
+
+    def __init__(
+            self,
+            num_states=100,
+            num_actions=4,
+            alpha=0.2,
+            gamma=0.9,
+            rar=0.5,
+            radr=0.99,
+            dyna=0,
+            verbose=False,
+    ):
         """  		  	   		  		 			  		 			     			  	 
         Constructor method  		  	   		  		 			  		 			     			  	 
-        """  		  	   		  		 			  		 			     			  	 
-        self.verbose = verbose  		  	   		  		 			  		 			     			  	 
-        self.num_actions = num_actions  		  	   		  		 			  		 			     			  	 
-        self.s = 0  		  	   		  		 			  		 			     			  	 
+        """
+        self.verbose = verbose
+        self.num_actions = num_actions
+        self.s = 0
         self.a = 0
 
         self.alpha = alpha
@@ -91,11 +92,14 @@ class QLearner(object):
         temp_state = rand.choice(list(self.previous.keys()))
         temp_action = rand.choice(self.previous[temp_state])
 
-        self.update(temp_state, temp_action, np.argmax(self.t_table[temp_state, temp_action]), self.r_table[temp_state, temp_action], 0)
+        self.update(temp_state, temp_action, np.argmax(self.t_table[temp_state, temp_action]),
+                    self.r_table[temp_state, temp_action], 0)
 
     def update(self, s, a, s_prime, r, table):
         if table == 0:
-            self.q_table[s, a] = (-self.alpha * (np.max(self.q_table[s_prime]) * -self.gamma - r) - self.q_table[s, a] * (self.alpha - 1))
+            self.q_table[s, a] = (
+                        -self.alpha * (np.max(self.q_table[s_prime]) * -self.gamma - r) - self.q_table[s, a] * (
+                            self.alpha - 1))
         elif table == 1:
             self.r_table[s, a] = self.alpha * r - (self.alpha - 1) * self.r_table[s, a]
 
@@ -121,11 +125,10 @@ class QLearner(object):
 
         return action
 
-
     def query(self, s_prime, r):
         """  		  	   		  		 			  		 			     			  	 
         Update the Q table and return an action  		  	   		  		 			  		 			     			  	 
-  		  	   		  		 			  		 			     			  	 
+
         :param s_prime: The new state  		  	   		  		 			  		 			     			  	 
         :type s_prime: int  		  	   		  		 			  		 			     			  	 
         :param r: The immediate reward  		  	   		  		 			  		 			     			  	 
@@ -141,7 +144,6 @@ class QLearner(object):
             self.t_table[self.s, self.a, s_prime] += 1
             self.update(self.s, self.a, s_prime, r, 1)
 
-
             for i in range(self.dyna):
                 self.hallucinate()
 
@@ -151,6 +153,7 @@ class QLearner(object):
 
         return action
 
+
 if __name__ == "__main__":
-    #print("Remember Q from Star Trek? Well, this isn't him")
+    # print("Remember Q from Star Trek? Well, this isn't him")
     pass

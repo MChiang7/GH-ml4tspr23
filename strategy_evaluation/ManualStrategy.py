022a599261f76a0e10a69a4b3f9c27ee90caabbe
MChiang7 <mtchiang117@gmail.com>
2023-04-15 23:48:02 -0400

    manual strategy

--- strategy_evaluation/ManualStrategy.py
+++ strategy_evaluation/ManualStrategy.py
@@ -1,77 +1,167 @@
-import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import marketsimcode as mkt_sim
 import datetime as dt
-from util import plot_data, get_data
+from util import get_data
+from strategy_evaluation import indicators
 
-class TheoreticallyOptimalStrategy(object):
+
+class ManualStrategy(object):
     def __init__(self):
         pass
 
-    def testPolicy(self, symbol='JPM', sd=dt.datetime(2008, 1, 1), ed=dt.datetime(2009,12,31), sv=100000):
+    def author(self):
+        return 'mchiang30'
+
+    def testPolicy(self, symbol='JPM', sd=dt.datetime(2008, 1, 1), ed=dt.datetime(2009,12,31)):
         prices = get_data([[symbol][0]], pd.date_range(sd, ed))[[[symbol][0]]].ffill().bfill()
         trades = get_data([[symbol][0]], pd.date_range(sd, ed))[['SPY']].rename(columns={'SPY':[symbol][0]})
-        temp = 0
+        normalized_prices = (prices[symbol] / prices[symbol][0]).to_frame()
 
         for col in trades.columns:
             trades[col].values[:] = 0
 
-        for i in range(len(trades.index) - 1):
-            if prices.loc[trades.index[i]].loc[[symbol][0]] >= prices.loc[trades.index[i + 1]].loc[[symbol][0]]:
+        ema = indicators.exponential_moving_average(sd, ed, symbol, window_size=20, gen_plot=False)
+        gx_s, gx_l = indicators.goldenx(sd, ed, symbol, window_size=20, gen_plot=False)
+        macd_raw, macd_signal = indicators.macd(sd, ed, symbol, gen_plot=False)
+        momentum = indicators.momentum(sd, ed, symbol, gen_plot=False)
+        bb = indicators.bollinger_band(sd, ed, symbol, window_size=20, gen_plot=False)
+
+        normalized_gx_s = gx_s[symbol] / gx_s[symbol][0]
+        normalized_gx_l = gx_l[symbol] / gx_l[symbol][0]
+
+        temp_action = 0
+        temp_pos = 0
+
+        for i in range(len(trades.index)):
+            temp_action += 1
+
+            if ema.loc[trades.index[i]].loc[symbol] < normalized_prices.loc[trades.index[i]].loc[symbol]:
+                ema_action = 1
+            elif ema.loc[trades.index[i]].loc[symbol] > normalized_prices.loc[trades.index[i]].loc[symbol]:
+                ema_action = -1
+            else:
+                ema_action = 0
+
+            if bb.loc[trades.index[i]].loc[symbol] < 0.0:
+                bb_action = 1
+            elif momentum.loc[trades.index[i]].loc[symbol] > 1:
+                bb_action = -1
+            else:
+                bb_action = 0
+
+            if momentum.loc[trades.index[i]].loc[symbol] < -0.5:
+                momentum_action = 1
+            elif momentum.loc[trades.index[i]].loc[symbol] > 0.5:
+                momentum_action = -1
+            else:
+                momentum_action = 0
+
+            if macd_signal.loc[trades.index[i]].loc[symbol] < macd_raw.loc[trades.index[i]].loc[symbol]:
+                macd_action = 1
+            elif macd_signal.loc[trades.index[i]].loc[symbol] > macd_raw.loc[trades.index[i]].loc[symbol]:
+                macd_action = -1
+            else:
+                macd_action = 0
+
+            if normalized_gx_l.loc[trades.index[i]] > normalized_gx_s.loc[trades.index[i]]:
+                goldenx_action = -1
+            elif normalized_gx_l.loc[trades.index[i]] < normalized_gx_s.loc[trades.index[i]]:
+                goldenx_action = 1
+            else:
+                goldenx_action = 0
+
+            if goldenx_action + macd_action + momentum_action + bb_action + ema_action < 0:
+                sign = 1
+            elif goldenx_action + macd_action + momentum_action + bb_action + ema_action > 0:
                 sign = -1
             else:
-                sign = 1
-            trades.loc[trades.index[i]].loc[[symbol][0]] = sign * 1000 - temp
-            temp -= (sign * 1000 - temp) * -1
+                sign = 0
+
+            if temp_action >= 5:
+                trades.loc[trades.index[i]].loc[symbol] = (temp_pos - sign * 1000) * -1
+                temp_pos += (temp_pos - sign * 1000) * -1
+                temp_action = 0
+
         return trades
 
-def author(self):
-    return 'mchiang30'
 
-def output(sv, sd, ed, symbol, gen_stats=False, gen_plot=False):
-    trades = get_data(['SPY'], pd.date_range(sd, ed)).rename(columns={'SPY':'JPM'})
+def benchmark(symbol, sd, ed, sv, commission, impact):
+    trades = get_data(['SPY'], pd.date_range(sd, ed)).rename(columns={'SPY': symbol})
     for col in trades.columns:
         trades[col].values[:] = 0
     trades.loc[trades.index[0]] = 1000
+    val = mkt_sim.compute_portvals(trades, start_val=sv, commission=commission, impact=impact)
+    return val
 
-    benchmark_val = mkt_sim.compute_portvals(trades, sv, commission=0.00, impact=0.00)
-    theoretical_val = mkt_sim.compute_portvals(TheoreticallyOptimalStrategy().testPolicy(symbol, sd=sd, ed=ed, sv=sv), sv, commission=0.00, impact=0.00)
+def gen_plot(manual_val, benchmark_val, short_entries, long_entries, sample, symbol):
+    pd.plotting.register_matplotlib_converters()
 
-    if gen_plot:
-        plt.figure(figsize=(12, 9))
-        plt.title('Theoretically Optimal Strategy vs Benchmark for JPM')
-        plt.xlabel('Dates')
-        plt.xticks(rotation=25)
-        plt.ylabel('Normalized value')
+    plt.figure(figsize=(14, 8))
+    plt.title('Manual Strategy vs Benchmark - {} ({})'.format(sample, symbol))
+    plt.xlabel("Date")
+    plt.ylabel("Cumulative Return")
+    plt.xticks(rotation=25)
+    plt.grid()
+    plt.plot(manual_val, label='Manual Strategy', color='red')
+    plt.plot(benchmark_val, label='Benchmark', color='purple')
+    for i in long_entries:
+        plt.axvline(i, color="blue")
+    for i in short_entries:
+        plt.axvline(i, color="black")
+    plt.legend()
+    plt.savefig("./images/manual_{}.png".format(sample))
+    plt.clf()
 
-        plt.plot(theoretical_val['value'] / theoretical_val['value'][0], label='theoretical', color='red')
-        plt.plot(benchmark_val['value'] / benchmark_val['value'][0], label='benchmark', color='purple')
+def entries(trades, symbol):
+    short_entries = []
+    long_entries = []
 
-        plt.legend()
-        plt.grid()
+    temp = 0
+    action = 'init'
+    for i in trades.index:
+        temp += trades.loc[i].loc[symbol]
+        if temp > 0:
+            if action == -1 or action == 'init':
+                long_entries.append(i)
+                action = 1
+        elif temp < 0:
+            if action == 1 or action == 'init':
+                short_entries.append(i)
+                action = -1
 
-        plt.savefig('./images/tos_b_comp.png')
-        #plt.show()
-        plt.clf()
+    return short_entries, long_entries
 
-    if gen_stats:
-        benchmark_cum_ret = benchmark_val['value'][-1] / benchmark_val['value'][0] - 1
-        benchmark_std_daily_ret = ((benchmark_val['value'] / benchmark_val['value'].shift(1) - 1).iloc[1:]).std()
-        benchmark_mean_daily_ret = ((benchmark_val['value'] / benchmark_val['value'].shift(1) - 1).iloc[1:]).mean()
+def report(symbol='JPM',
+                sd_in=dt.datetime(2008,1,1),
+                ed_in=dt.datetime(2009,12,31),
+                sd_out=dt.datetime(2010, 1, 1),
+                ed_out=dt.datetime(2011, 12, 31),
+                sv=100000,
+                commission=9.95,
+                impact=0.005):
 
-        theoretical_cum_ret = theoretical_val['value'][-1] / theoretical_val['value'][0] - 1
-        theoretical_std_daily_ret = ((theoretical_val['value'] / theoretical_val['value'].shift(1) - 1).iloc[1:]).std()
-        theoretical_mean_daily_ret = ((theoretical_val['value'] / theoretical_val['value'].shift(1) - 1).iloc[1:]).mean()
+    benchmark_val = benchmark(symbol, sd_in, ed_in, sv, commission, impact)
+    trades = ManualStrategy().testPolicy(symbol, sd=sd_in, ed=ed_in)
+    manual_val = mkt_sim.compute_portvals(trades, sv, commission=commission, impact=impact)
 
-        d = [['{:.6f}'.format(theoretical_cum_ret), '{:.6f}'.format(benchmark_cum_ret)],
-                 ['{:.6f}'.format(theoretical_std_daily_ret), '{:.6f}'.format(benchmark_std_daily_ret)],
-                 ['{:.6f}'.format(theoretical_mean_daily_ret), '{:.6f}'.format(benchmark_mean_daily_ret)]]
-        strats = ['TOS', 'Benchmark']
-        metric = ['Cumulative Return', 'Stdev of daily returns', 'Mean of daily returns']
-        df_tb_stats = pd.DataFrame(d, metric, strats)
-        print(df_tb_stats.to_string())
+    benchmark_val['value'] = benchmark_val['value'] / benchmark_val['value'][0]
+    manual_val['value'] = manual_val['value'] / manual_val['value'][0]
 
+    short_entries, long_entries = entries(trades, symbol)
 
-if __name__ == '__main__':
+    gen_plot(manual_val, benchmark_val, short_entries, long_entries, 'In Sample', symbol)
+
+    benchmark_val = benchmark(symbol, sd_out, ed_out, sv, commission, impact)
+    trades = ManualStrategy().testPolicy(symbol, sd=sd_out, ed=ed_out)
+    manual_val = mkt_sim.compute_portvals(trades, sv, commission=commission, impact=impact)
+
+    benchmark_val['value'] = benchmark_val['value'] / benchmark_val['value'][0]
+    manual_val['value'] = manual_val['value'] / manual_val['value'][0]
+
+    short_entries, long_entries = entries(trades, symbol)
+
+    gen_plot(manual_val, benchmark_val, short_entries, long_entries, 'Out Sample', symbol)
+
+if __name__ == "__main__":
     pass
